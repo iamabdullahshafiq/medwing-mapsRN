423af961191de4f1dd2ad042b1661fecbceab2f5
Muhammad Abdullah Shafiq <iamabdullahshafiq@gmail.com>
2019-03-15 00:12:15 +0500

    Added Modal for create Location Form

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {
+  Modal,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  Alert,
+  TextInput
+} from 'react-native';
 import { MapView } from 'expo';
 
 const markers = [
@@ -30,29 +38,81 @@ export default class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { markers };
+    this.state = { markers, modalVisible: false, text: 'Useless Placeholder' };
+  }
+
+  createLocation(position) {
+    const { markers } = this.state;
+    console.log(position);
+    const latlng = position.nativeEvent.coordinate;
+    const marker = {
+      id: markers[markers.length - 1].id + 1,
+      latlng,
+      title: 'temp',
+      description: 'temp'
+    };
+    markers.push(marker);
+    this.setState({ markers });
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
   }
 
   render() {
     return (
-      <MapView
-        style={styles.container}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      >
-        {this.state.markers.map(marker => (
-          <MapView.Marker
-            key={marker.id}
-            coordinate={marker.latlng}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-      </MapView>
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}
+        >
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => this.setState({ text })}
+                value={this.state.text}
+              />
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}
+              >
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+        <MapView
+          style={styles.container}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+          onLongPress={position => {
+            this.setModalVisible(true);
+            this.createLocation(position);
+          }}
+        >
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              key={marker.id}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
+      </View>
     );
   }
 }
